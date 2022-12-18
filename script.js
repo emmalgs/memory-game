f046f75f40c2fcb6a3a0ddef4a5392135147b1a3
Emma Gerigscott <elgerig@gmail.com>
2022-12-18 18:05:37 -0500

    create board

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
 const cardArray = [
     {
         name: 'fries',
-        img: '../images/fries.png'
+        img: 'images/fries.png'
     },
     {
         name: 'cheeseburger',
-        img: '../images/cheeseburger.png'
+        img: 'images/cheeseburger.png'
     },
     {
         name: 'hotdog',
-        img: '../images/hotdog.png'
+        img: 'images/hotdog.png'
     },
     {
         name: 'ice-cream',
-        img: '../images/ice-cream.png'
+        img: 'images/ice-cream.png'
     },
     {
         name: 'milkshake',
-        img: '../images/milkshake.png'
+        img: 'images/milkshake.png'
     },
     {
         name: 'pizza',
-        img: '../images/pizza.png'
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
     }
 ]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard() {
+    for (let i = 0; i < 10; i++) {
+       const card = document.createElement('img')
+       card.setAttribute('src', 'images/blank.png')
+       card.setAttribute('data-id', i)
+       gridDisplay.append(card)
+    }
+}
+
+createBoard()
